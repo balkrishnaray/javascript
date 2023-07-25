00f4e58bdea25c3ca4d2b54da2d2b810a6bdaa2a
balkrishnray <balkrishnaroy31@gmail.com>
2023-07-25 20:47:48 +0545

    dom searching

---
+++ SearchingDom/script.js
@@ -0,0 +1,14 @@
+let cTitle = document.getElementById("first-card");
+cTitle.style.color = "green";
+
+let cTitles = document.querySelectorAll(".card-title");
+cTitles[0].style.color = "red";
+cTitles[1].style.color = "green";
+cTitles[2].style.color = "blue";
+
+document.querySelector(".this").style.color = "white";
+document.querySelector(".this").style.background = "red";
+document.querySelector(".this1").style.color = "white";
+document.querySelector(".this1").style.background = "green";
+document.querySelector(".this2").style.color = "white";
+document.querySelector(".this2").style.background = "blue";
